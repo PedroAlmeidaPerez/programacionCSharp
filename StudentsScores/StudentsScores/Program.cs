1cb8b3ea452af01c9aa049943a226f1d7ba5a8c7
A681934 <pedro.perez@atos.net>
2023-04-26 11:43:26 +0200

    studentsScores finish

--- StudentsScores/StudentsScores/Program.cs
+++ StudentsScores/StudentsScores/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -8,25 +9,81 @@ namespace StudentsScores
     {
         static void Main(string[] args)
         {
-            int score;
+            
             string[]  array1 = { "Pedro", "Pablo", "Bianka", "Oliwia"};
+           
+            
+            //insertScore(array1 as string[]);
             SortedList<string, int> sortedStudentsScores = new SortedList<string, int>();
-            
-            ShowArray(array1 as string[]);
+            int score;
+            Console.WriteLine("dentro de ShowArray");
 
-            
+            foreach (string s in array1)
+            {
+                Console.WriteLine($"Proporciona la nota del estudiante {s}");
+                score = 0;
+                while (score == 0)
+                {
+                    GetInteger(Console.ReadLine(), out score);
+                }
+
+                sortedStudentsScores.Add(s, score);
+
+            }
+
+            foreach (var item in sortedStudentsScores)
+            {
+                Console.WriteLine($"El estudiante {item.Key} tiene la nota {item.Value}");
+            }
+
+           
+
+
+
+
+
+
 
             Console.WriteLine("Fin del programa");
             Console.ReadKey();
         }
 
-        private static void ShowArray(string[] strings)
+        /*private static void GetInteger(string v, object data, out int score1)
         {
+            if(!int.TryParse(v, out score1))
+            {
+                Console.WriteLine("el dato debe ser entero");
+            }
+        }*/
+
+       /* private sortedStudentsScores<Object> insertScore(string[] strings)
+        {
+            SortedList<string, int> sortedStudentsScores = new SortedList<string, int>();
+            int score;
             Console.WriteLine("dentro de ShowArray");
+           
             foreach (string s in strings)
             {
-                Console.WriteLine(s);
+                Console.WriteLine($"Proporciona la nota del estudiante {s}");
+                score = 0;
+                while (score == 0)
+                {
+                    GetInteger(Console.ReadLine(), out score);
+                }
+
+                sortedStudentsScores.Add(s, score);
+
             }
+
+            return sortedStudentsScores;
+
+
+        }*/
+
+        private static void GetInteger(string data, out int score)
+        {
+            if (!int.TryParse(data, out score))
+                Console.WriteLine("el dato debe ser un entero");
         }
     }
 }
